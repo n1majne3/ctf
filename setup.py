bb089c528ee2fef3869cf361f5ddc873a552e686
n1majne3 <benjamin-liang@outlook.com>
2023-05-15 09:25:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,17 @@
-import os
-os.system('cat /flag')
+from setuptools import setup，find_packages
+
+setup(
+    name="testing",
+    version="",
+    author="",
+    author_email="",
+    description=(""),
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=['],
+    license="MIT",
+    packages=find_packages(),
+    url="https://github.com/n1majne3/ctf",
+)
+
+
